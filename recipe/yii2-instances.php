b15f7e2c02468dcac2c0fcb3e3dd951492515810
Alexander Kryshtalev <alex@voodoo-mobile.com>
2016-04-06 10:59:07 +0600

    [*] Added checking for production server when copying the database

--- recipe/yii2-instances.php
+++ recipe/yii2-instances.php
@@ -1,23 +1,26 @@
 <?php
 
 use Deployer\Database\Database;
-use Deployer\Deployer;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
 option('from', null, InputOption::VALUE_OPTIONAL, 'The source server for copying the instance');
 
-task('copy:dump', function () {
+task('copy:database', function () {
     if (!input()->getOption('from')) {
         throw new Exception('The source server is required');
     }
 
-    $to = Database::loadUsingStage(input()->getArgument('stage'));
+    $from = input()->getArgument('stage');
+    if ($from == 'production') {
+        throw new \Exception('That is strictly prohibited to restore the dump to the production server');
+    }
 
-    Database::loadUsingStage(input()->getOption('from'))->restore($to->dump());
+    /** @var string $dump */
+    $dump = Database::loadUsingStage($from)->dump();
+    Database::loadUsingStage(input()->getOption('from'))->restore($dump);
+
+    run("rm -f " . $dump);
 });
 
-task('copy:restore', function () {
-});
-
-task('copy', ['copy:dump', 'copy:restore']);
+task('copy', ['copy:database']);
