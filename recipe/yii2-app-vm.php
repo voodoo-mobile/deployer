5e1b36d8c36237330356fa4c474b7cba920f5b6f
Alexander Kryshtalev <alex@voodoo.rocks>
2017-04-17 14:39:47 +0200

    [+] Added slim support

--- recipe/yii2-app-vm.php
+++ recipe/yii2-app-vm.php
@@ -1,8 +1,6 @@
 <?php
 
-use Symfony\Component\Console\Input\InputOption;
-
-require_once 'yii2-app-basic.php';
+require_once __DIR__ . '/common-vm.php';
 
 set('shared_dirs', [
     'runtime',
@@ -10,42 +8,11 @@ set('shared_dirs', [
     'web/uploads',
 ]);
 
-set('writable_use_sudo', true);
-set('default_branch', 'develop');
+/**
+ * Run migrations
+ */
+task('deploy:run_migrations', function () {
+    run('php {{release_path}}/yii migrate up --interactive=0');
+})->desc('Run migrations');
 
-env('branch', function () {
-    if (input()->hasOption('branch')) {
-        return input()->getOption('branch');
-    } else {
-        return get('default_branch');
-    }
-});
-
-option('branch', null, InputOption::VALUE_OPTIONAL, 'Branch to deploy.');
-
-task('deploy:publish', function () {
-    $dirs = get('shared_dirs');
-    foreach ($dirs as $dir) {
-        run("mkdir -p {{release_path}}/{$dir} && sudo chmod -R 777 {{release_path}}/{$dir}");
-    }
-    run("cd {{release_path}} && ln -sfn {{release_path}}/web /var/www/{{project}}");
-
-    $stages = env('stages');
-    foreach ($stages as $stage) {
-        run("cd {{release_path}}/web && [[ -e index-{$stage}.php ]] && cp -f index-{$stage}.php index.php");
-    }
-})->desc('Publishing to www');
-
-task('deploy:prerequisites', function () {
-    $stages = env('stages');
-    foreach ($stages as $stage) {
-        run("cd {{release_path}} && touch {$stage}");
-    }
-});
-
-task('deploy:compile', function () {
-    run("cd {{release_path}} && npm install && npm run-script production");
-});
-
-after('deploy:update_code', 'deploy:prerequisites');
-before('cleanup', 'deploy:publish');
+after('deploy:vendors', 'deploy:run_migrations');
